b367e3c99058ac121a81c35a65e93217fa4f7029
Nicolas Maupu <nmaupu@gmail.com>
2010-11-18 02:24:43 +0100

    Adding interactive behavior Error handling by Either Monad

--- RPN.hs
+++ RPN.hs
@@ -1,19 +1,53 @@
-import Data.Maybe
-import Control.Applicative
+import Data.Either
 
-foldingFunc :: [Maybe Double] -> String -> [Maybe Double]
-foldingFunc []  [] = []
-foldingFunc xs  [] = xs
-foldingFunc []  s  = (Just . read $ s):[]
-foldingFunc [x] s  = (Just . read $ s):[x]
-foldingFunc (Just 0:_:xs) "/" = [Nothing]
-foldingFunc lst@(a:b:xs) s 
-  | s == "+"            = (pure (+) <*> b <*> a):xs
-  | s == "-"            = (pure (-) <*> b <*> a):xs
-  | s == "/"            = (pure (/) <*> b <*> a):xs
-  | s == "*"            = (pure (*) <*> b <*> a):xs
-  | s == "sum"          = [Just . sum $ fmap fromJust lst]
-  | otherwise           = (Just . read $ s):lst
+isLeft :: Either a b -> Bool
+isLeft (Left a) = True
+isLeft _        = False
 
-computeRPN :: String -> Maybe Double
-computeRPN s = head . foldl foldingFunc [] $ words s
+printStack :: Either String [Double] -> IO ()
+printStack (Right x) = putStrLn $ "Stack = " ++ (show x)
+printStack (Left x)  = putStrLn $ x
+
+printLastValue :: Either String [Double] -> IO ()
+printLastValue (Left x)  = putStrLn x
+printLastValue (Right x) = putStrLn . show . head $ x
+
+printLastError :: Either String [Double] -> IO ()
+printLastError e@(Left x) = printLastValue e
+printLastError _ = return ()
+
+
+push :: String -> Either String [Double] -> Either String [Double]
+push a (Right [])   = Right $ [(read a)]
+push a (Right [x])  = Right $ (read a):[x]
+push "/" (Right (0:xs)) = Left "Error: divide by zero"
+push a (Right lst@(x:y:xs)) = case a of
+  "+"   -> Right $ ((+) y x):xs
+  "-"   -> Right $ ((-) y x):xs
+  "/"   -> Right $ ((/) y x):xs
+  "*"   -> Right $ ((*) y x):xs
+  "sum" -> Right $ [(sum lst)]
+  _     -> Right $ (read a):lst
+
+pop :: Either String [Double] -> Either String [Double]
+pop (Right (x:xs)) = Right xs
+
+
+mainLoop :: Either String [Double] -> IO()
+mainLoop stack = do
+  entry <- getLine
+  st <- case entry of
+          "P" -> do printLastValue stack
+                    return stack
+          "p" -> do printLastValue stack
+                    return . pop $ stack
+          "f" -> do printStack stack
+                    return stack
+          _   -> return $ push entry stack
+
+  
+  printLastError st
+  mainLoop (if isLeft st then stack else st)
+
+main = do
+  mainLoop $ Right []
